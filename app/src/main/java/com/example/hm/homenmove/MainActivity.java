e1ae92d26773a9a68d9a90a76578cc10c73d21af
CelinePinaglia <celine.pinaglia@yahoo.fr>
2016-05-22 19:04:19 +0200

    MainActivity design

--- app/src/main/java/com/example/hm/homenmove/MainActivity.java
+++ app/src/main/java/com/example/hm/homenmove/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.hm.homenmove;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView monRecyclerView;
+    private RecyclerView.Adapter monAdapter;
+    private RecyclerView.LayoutManager monLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        monRecyclerView = (RecyclerView) findViewById(R.id.recycleView);
+
+        monLayoutManager = new LinearLayoutManager(this);
+        monRecyclerView.setLayoutManager(monLayoutManager);
+
+        String[] monDataSet = {"1","2","3","4"};
+        monAdapter = new AdapterMain(getBaseContext(), monDataSet);
+        monRecyclerView.setAdapter(monAdapter);
     }
 }
