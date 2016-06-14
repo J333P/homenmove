d4720ab171532a63a1528c7403963cd4e6ed49fc
Khaderik <hrdcedric@gmail.com>
2016-06-15 00:39:23 +0200

    tests clientApi

--- app/src/main/java/com/example/hm/homenmove/MainActivity.java
+++ app/src/main/java/com/example/hm/homenmove/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.hm.homenmove;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import com.example.hm.homenmove.modeles.Chambre;
+import com.homenmove.api.wshnm.HomeNMoveClient;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,17 +19,43 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter monAdapter;
     private RecyclerView.LayoutManager monLayoutManager;
 
+
+    private Call<List<Chambre>> monCallListeDesChambres;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         monRecyclerView = (RecyclerView) findViewById(R.id.recycleViewChambre);
 
-        monLayoutManager = new LinearLayoutManager(this);
-        monRecyclerView.setLayoutManager(monLayoutManager);
+        monCallListeDesChambres = HomeNMoveClient.getChambresSvc().getLesChambres();
 
-        String[] monDataSet = {"1","2","3","4"};
-        monAdapter = new AdapterMain(getBaseContext(), monDataSet);
-        monRecyclerView.setAdapter(monAdapter);
+        monCallListeDesChambres.enqueue(new Callback<List<Chambre>>() {
+            @Override
+            public void onResponse(Call<List<Chambre>> call, Response<List<Chambre>> response) {
+                int statusCode = response.code();
+                List<Chambre> mesChambres  = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<List<Chambre>> call, Throwable t) {
+                // Log error here since request failed
+
+            }
+        });
+
+//            monLayoutManager=new
+//
+//            LinearLayoutManager(this);
+//
+//            monRecyclerView.setLayoutManager(monLayoutManager);
+//
+//            String[] monDataSet = {"1", "2", "3", "4"};
+//            monAdapter=new
+//
+//            AdapterMain(getBaseContext(),monDataSet
+//
+//            );
+//            monRecyclerView.setAdapter(monAdapter);
+        }
     }
-}
